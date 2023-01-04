9fc6f4435242efb78b53a4c0d0f4ee1fe629903b
codepanther <pezechigozirim@gmail.com>
2023-01-04 13:15:03 +0100

    Set up application to use Next 12 experimental appDir

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Codepantha Live News</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
