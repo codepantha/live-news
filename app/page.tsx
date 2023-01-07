c8de3f7438d9e46a5b58e14432cfb1d5184beb8a
codepanther <pezechigozirim@gmail.com>
2023-01-07 13:17:36 +0100

    Display news articles

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
+
 import { categories } from '../utils/constants'
 import fetchNews from '../utils/fetchNews';
+import NewsList from './NewsList';
+import data from '../data.json';
+import sortNewsByImage from '../utils/sortNewsByImage';
 
 const Homepage = async () => {
   // fetch the news data
-  const news: NewsResponse = await fetchNews(categories.join(','));
+  const news: NewsResponse = sortNewsByImage(data) || await fetchNews(categories.join(','));
   console.log(news);
   return (
     <div>
-      {/* Newslist list */}
+      <NewsList news={news} />
     </div>
   )
 }
