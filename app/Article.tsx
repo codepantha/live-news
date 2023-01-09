93ee5a3a604616f9c952fb3346a2aaf0a9f66fa2
codepanther <pezechigozirim@gmail.com>
2023-01-09 13:20:05 +0100

    Display default image when news image not available

--- app/Article.tsx
+++ app/Article.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 import ReadMoreButton from './ReadMoreButton';
 
@@ -7,14 +8,23 @@ type Props = {
 
 const Article = ({ article }: Props) => {
   return (
-    <article className="bg-slate-100 dark:bg-slate-800 flex
+    <article
+      className="bg-slate-100 dark:bg-slate-800 flex
       flex-col rounded-lg shadow-sm hover:scale-105 hover:shadow-lg
       hover:bg-slate-200 transition-all duration-200 ease-out"
     >
-      {article.image && (
+      {article.image ? (
         <img
           src={article.image}
           alt={article.title}
+          className="h-55 w-full object-cover rounded-t-lg shadow-md"
+        />
+      ) : (
+        <Image
+          src="/img-placeholder.jpg"
+          alt="random image"
+          width={100}
+          height={100}
           className="h-55 w-full object-cover rounded-t-lg shadow-md"
         />
       )}
@@ -27,9 +37,10 @@ const Article = ({ article }: Props) => {
             <p className="text-xs line-clamp-2">{article.description}</p>
           </section>
 
-          <footer className="text-xs text-right ml-auto flex space-x-1 
+          <footer
+            className="text-xs text-right ml-auto flex space-x-1 
             pt-5 static text-gray-400"
-           >
+          >
             <p>{article.source}</p>
             <p>{article.published_at}</p>
           </footer>
